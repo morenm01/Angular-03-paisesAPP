494200039ba7652ad0368739e9ca4026b4763271
miriam <m.moreno.clavijo@gmail.com>
2021-10-11 17:20:49 +0200

    añadiendo rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -11,8 +12,10 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     PaisModule,
     SharedModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
